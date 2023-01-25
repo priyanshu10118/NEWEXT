2af892c425505dc077da11b24b35e74510b95563
pratay roy <2028026@kiit.ac.in>
2023-01-25 22:37:33 +0530

    Added NavBar HeroCarousel StockMarketToday WeatherLoc

--- newext-frontend/src/components/Homepage.jsx
+++ newext-frontend/src/components/Homepage.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import HeroCarousel from "./homepage/HeroCarousel"
+import WeatherLoc from "./homepage/WeatherLoc";
+import Navbar from "./homepage/Navbar";
+import StockMarketToday from "./homepage/StockMarketToday";
 
 export default function Homepage() {
-  return <div>home page</div>;
+  return (
+    <div>
+      <div className="section-navbar">
+        <Navbar /> 
+      </div>
+      <div className="section-hero">
+        <HeroCarousel />
+        <div className="section-hero--right">
+          <WeatherLoc />
+          <StockMarketToday />
+        </div>
+      </div>
+    </div>
+  )
 }
--- newext-frontend/src/components/homepage/MainCarousel.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function MainCarousel() {
-  return <div>Main Carousel here</div>;
-}
