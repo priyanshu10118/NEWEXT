da1ac593365b881b210d6b99a5a0565781a15151
manishmh <manishmh982@gmail.com>
2023-03-01 00:29:49 +0530

    uploading header folder with css file

--- newext-frontend/src/components/homepage/Header/Header.jsx
+++ newext-frontend/src/components/homepage/Header/Header.jsx
@@ -1,30 +1,48 @@
 import React from 'react'
+import './header.css'
 import 'font-awesome/css/font-awesome.min.css'
 
-export default function Header() 
-{
+const SearchBar = () => {
   return (
+    <>
+      <form className="search-bar" action="">
+        <div className="search-button">
+          <i className="fa fa-search" onClick={() => {}}></i>
+        </div>
+        <div className="search-input-container">
+          <input type="text" placeholder="Search..." name="search-bar" onClick={() => {}}/>
+        </div>
+      </form>
+    </>
+  )
+}
+
+const ProfileLogo = () => {
+  return (
+    <div className="my-account" onClick={() => {}}>
+      <i 
+        className="fa fa-user-o" >
+      </i>
+    </div>
+  )
+}
+
+const Header = () => {
+  return (
+      <>
       <div className="header-container">
         <div className="header-logo-container">
           <img src="https://i.postimg.cc/htmNRqtz/newext-logo.png" className="newext-logo" alt="newext-logo" />
         </div>
         <div className="search-and-account-container">
           <div className="search-bar-container">
-            <form className="search-bar" action="">
-              <input 
-                type="text" 
-                placeholder="Search..." 
-                name="search-bar" 
-                onClick={() => {}}
-              />
-              <i 
-                className="fa fa-search search-logo" 
-                onClick={() => {}}>
-              </i>
-            </form>
+            <SearchBar />
           </div>
-          <div className="my-account">My account</div>
+          <ProfileLogo />
         </div> 
       </div>
+    </>
   )
 }
+
+export default Header
