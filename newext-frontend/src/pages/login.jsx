abe75115cb7ff07df0dbc21a10bf9b4fcd917128
priyansh pandey <priyanshpandey2055@gmail.com>
2023-03-03 23:25:00 +0530

    functioning login and signup

--- newext-frontend/src/pages/login.jsx
+++ newext-frontend/src/pages/login.jsx
@@ -1,10 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/Login.css";
 import logo from "../assets/logo.png";
 import leftLogo from "../assets/support-team 1 (1).png";
 import google from "../assets/Group 124.png";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+    badgeImg: "",
+  });
+
+  // google client id = 454265286170-g78tl5j8fpv9j6dbel5n1cu4a4cv2jqn.apps.googleusercontent.com
+  // google client secreat = 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
+
+  const signup = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      try {
+        const response = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${tokenResponse.access_token}`,
+            },
+          }
+        );
+        user.fname = response.data.given_name;
+        user.lname = response.data.family_name;
+        user.email = response.data.email;
+        user.password = response.data.sub;
+        user.badgeImg = response.data.picture;
+
+        try {
+          const resp = await axios.post("http://localhost:9876/signup", user, {
+            withCredentials: true,
+          });
+          if (resp.data.user) {
+            console.log(resp.data.user);
+            setUser({
+              fname: "",
+              lname: "",
+              email: "",
+              password: "",
+              badgeImg: "",
+            });
+          }
+        } catch (error) {
+          alert(error);
+        }
+      } catch (error) {
+        alert(error);
+      }
+    },
+  });
+
+  const signin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      try {
+        const response = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${tokenResponse.access_token}`,
+            },
+          }
+        );
+        user.email = response.data.email;
+        user.password = response.data.sub;
+
+        try {
+          const resp = await axios.post("http://localhost:9876/signin", user, {
+            withCredentials: true,
+          });
+          if (resp.data.user) {
+            alert("Signin Successfull ", resp.data.fname);
+          } else {
+            alert("Invalid credentials");
+          }
+        } catch (error) {
+          alert(error);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
   return (
     <div className="mainContainer">
       <div className="leftContainer">
@@ -22,13 +107,13 @@ const Login = () => {
           </p>
         </div>
         <div className="signin">
-          <buttom className="signinButton" onclick="#">
+          <button className="signinButton" onClick={signin}>
             <img src={google} alt=""></img>
-          </buttom>
+          </button>
         </div>
         <div className="signup">
           <p>
-            Don’t have an account? <a href="#">Sign up</a>
+            Don’t have an account? <button onClick={signup}>Sign up</button>
           </p>
         </div>
       </div>
