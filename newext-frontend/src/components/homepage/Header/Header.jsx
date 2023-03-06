871cc930fc2a4bd5fa8d276268b5a7becba8d60d
manishmh <manishmh982@gmail.com>
2023-03-07 01:33:58 +0530

    added search filter in search bar

--- newext-frontend/src/components/homepage/Header/Header.jsx
+++ newext-frontend/src/components/homepage/Header/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useState, useMemo } from 'react'
 import './header.css'
 import 'font-awesome/css/font-awesome.min.css'
 
+
 const SearchBar = () => {
+  const [query, setQuery] = useState("")
+
+  // test JSON data
+  const Data = [
+    {title: 'Manish Malhotra', link: 'https://'},
+    {title: 'Harshit yadav', link: 'https;//'},
+    {title: 'aradhya kumar', link: 'https;//'},
+    {title: 'Abhinav Prasad', link: 'https;//'},
+    {title: 'Pratay Roy', link: 'https;//'},
+    {title: 'Anshul Kumar', link: 'https;//'},
+    {title: 'Anmol Agarwal', link: 'https;//'},
+    {title: 'Priyanshu Jha', link: 'https;//'},
+    {title: 'Shivangi Shrivastava', link: 'https;//'},
+    {title: 'Priyansh Pandey', link: 'https;//'}
+  ]
+
+  // going throudh JSON data and filtering out titles w.r.t keyword typed in search bar 'query'
+  // useMemo so other element than Data and query does not refresh when search value changes
+  const filteredData = useMemo(() => {
+    return Data.filter((val)=> {
+      if (query == "") {
+        return 
+      } else if (val.title.toLowerCase().includes(query.toLowerCase())) {
+        return val
+      }
+    })
+  }, [Data, query])
+
   return (
     <>
       <form className="search-bar" action="">
@@ -10,7 +40,15 @@ const SearchBar = () => {
           <i className="fa fa-search" onClick={() => {}}></i>
         </div>
         <div className="search-input-container">
-          <input type="text" placeholder="Search..." name="search-bar" onClick={() => {}}/>
+          <input type="search" placeholder="Search..." id='search'
+            value={ query } 
+            onChange={e => setQuery(e.target.value)} 
+          />
+          <div className="search-filter" id='search-filter'>
+            {filteredData.map(item => (
+              <div className="items-search"><a href={item.link}>{item.title}</a></div>
+            ))}
+          </div>
         </div>
       </form>
     </>
--- newext-frontend/src/main.jsx
+++ newext-frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="454265286170-g78tl5j8fpv9j6dbel5n1cu4a4cv2jqn.apps.googleusercontent.com">
+    {/* <GoogleOAuthProvider clientId="454265286170-g78tl5j8fpv9j6dbel5n1cu4a4cv2jqn.apps.googleusercontent.com"> */}
       <App />
-    </GoogleOAuthProvider>
+    {/* </GoogleOAuthProvider> */}
   </React.StrictMode>
 );
