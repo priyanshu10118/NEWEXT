0c3cc4ddce6580d965911131b42d7cef7e5e3c77
manishmh <manishmh982@gmail.com>
2023-04-01 20:41:25 +0530

    created single news page

--- newext-frontend/src/main.jsx
+++ newext-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
