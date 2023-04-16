5bf4f25f38a671ba5c99b821cf59738136efaf1d
AnmolAgarwal11 <anmol5.aa@gmail.com>
2023-04-17 01:47:34 +0530

    Stock market api integrated

--- newext-frontend/src/components/homepage/Hero/StockMarket/StockMarketTabs.jsx
+++ newext-frontend/src/components/homepage/Hero/StockMarket/StockMarketTabs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import './stockMarket.css'
-import { Apple, PlayArrow } from "@mui/icons-material";
+import "./stockMarket.css";
+import { PlayArrow } from "@mui/icons-material";
 
 export default function StockMarketTabs(props) {
 	return (
@@ -8,12 +8,11 @@ export default function StockMarketTabs(props) {
 			{/* Left content of Tabs */}
 
 			<div className="stockmarket-tabs--left">
-				<div className="stockmarket-tabs--logo">
-					<Apple />
-				</div>
 				<div className="stockmarket-tabs--name">
-					<h3>{props.name}</h3>
-					<p>{props.abbreviation}</p>
+					<h3>
+						{props.name.charAt(0).toUpperCase() + props.name.slice(1).toLowerCase()}
+					</h3>
+					<p>{[...props.abbreviation].map((e, index) => (index % 2 == 0 ? e : ""))}</p>
 				</div>
 			</div>
 
@@ -22,13 +21,12 @@ export default function StockMarketTabs(props) {
 			<div className="stockmarket-tabs--right">
 				<div className="stockmarket-tabs--right---upper">
 					<p className={`${props.percentClass}`}>
+						{props.changePercentages >= 0 ? "+" : ""}
 						{props.changePercentages}
 					</p>
-					<PlayArrow
-						className={`percent-arrow ${props.percentClass}`}
-					/>
+					<PlayArrow className={`percent-arrow ${props.percentClass}`} />
 				</div>
-				<h2>{props.currentPrices}</h2>
+				<h2>₹{props.currentPrices}</h2>
 			</div>
 		</div>
 	);
--- newext-frontend/src/components/homepage/Hero/Weather/WeatherLoc.jsx
+++ newext-frontend/src/components/homepage/Hero/Weather/WeatherLoc.jsx
@@ -4,54 +4,54 @@ import { ChevronRightIcon, SunIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
 
 export default function WeatherLoc() {
-  const [result, setResult] = useState();
-  const [date, setDate] = useState();
-  const [day, setDay] = useState();
-  const [month, setMonth] = useState();
-  const city = "raipur";
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  useEffect(() => {
-    const getWeatherReport = async () => {
-      const { data } = await axios.get(
-        `http://localhost:9876/weather/getweather?city=${city}`
-      );
-      setResult(data);
-      setDate(new Date().getDate());
-      setDay(days[new Date().getDay()]);
-      setMonth(months[new Date().getMonth()]);
-    };
-    getWeatherReport();
-  }, []);
-  return (
-    <div className="container-weatherloc">
-      <div className="weatherloc">
-        <div className="layout-flex">
-          <h6>{result?.name}</h6>
-          <ChevronRightIcon className="weatherloc-icon--more" />
-        </div>
-        <div className="layout-flex">
-          <p className="weatherloc-temperature">{Math.floor(result?.main.temp)}&#176;</p>
-          <div className="layout-flex--direction">
-            <p className="weatherloc-weather">{result?.weather[0].main}</p>
-            <p className="weatherloc-daydate">{`${day},${date} ${month}`}</p>
-          </div>
-          <SunIcon className="weatherloc-icon--weather" />
-        </div>
-      </div>
-    </div>
-  );
+	const [result, setResult] = useState();
+	const [date, setDate] = useState();
+	const [day, setDay] = useState();
+	const [month, setMonth] = useState();
+	const city = "raipur";
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	];
+	const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	useEffect(() => {
+		const getWeatherReport = async () => {
+			const { data } = await axios.get(
+				`http://localhost:9876/weather/getweather?city=${city}`
+			);
+			setResult(data);
+			setDate(new Date().getDate());
+			setDay(days[new Date().getDay()]);
+			setMonth(months[new Date().getMonth()]);
+		};
+		getWeatherReport();
+	}, []);
+	return (
+		<div className="container-weatherloc">
+			<div className="weatherloc">
+				<div className="layout-flex">
+					<h6>{result?.name}</h6>
+					<ChevronRightIcon className="weatherloc-icon--more" />
+				</div>
+				<div className="layout-flex">
+					<p className="weatherloc-temperature">{Math.floor(result?.main.temp)}&#176;</p>
+					<div className="layout-flex--direction">
+						<p className="weatherloc-weather">{result?.weather[0].main}</p>
+						<p className="weatherloc-daydate">{`${day},${date} ${month}`}</p>
+					</div>
+					<SunIcon className="weatherloc-icon--weather" />
+				</div>
+			</div>
+		</div>
+	);
 }
